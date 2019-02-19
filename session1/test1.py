3fae83e2543671006fbb6fb02a84b1fae79672e1
tungdau93 <tungdau1510@gmail.com>
2019-02-19 22:06:48 +0700

    hello

---
+++ session1/test1.py
@@ -0,0 +1,3 @@
+n = int(input("Nhap nam sinh: "))
+tuoi = 2019 - n
+print("Tuoi =", tuoi)
---
+++ session1/turtle_intro.py
@@ -0,0 +1,32 @@
+from turtle import *
+# khởi tạo con rùa
+shape('turtle')
+speed(0)
+
+# vòng lặp 500 lần
+# for i in range(500):
+#     for j in range(4):
+#         forward(100)
+#         left(90)
+        
+#     left(7)
+
+#chọn màu cho hình vẽ
+# color("red","yellow")
+# begin_fill()
+# for j in range(4):
+#     forward(100)
+#     left(90)
+# end_fill()
+
+#vẽ hình vuông nét đứt
+# for k in range(4):
+#     forward(50)
+#     penup()
+#     forward(50)
+#     pendown()
+#     left(90)
+
+# vẽ hình tròn
+# circle(100)
+mainloop()
