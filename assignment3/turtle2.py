42cfa8308dcedf1ea86d04bc6157db71e9709db0
tungdau93 <tungdau1510@gmail.com>
2019-03-04 11:27:36 +0700

    assignment3

--- assignment3/turtle2.py
+++ assignment3/turtle2.py
@@ -1,64 +1,64 @@
 from turtle import *
 
-# for i in range(1,6):
-#     color("red")
-#     begin_fill()
+for i in ["red", "blue", "brown", "yellow", "grey"]:
+    color(i)
+    begin_fill()
+    forward(50)
+    for i in range(2):
+        right(90)
+        forward(100)
+        right(90)
+        forward(50)
+    end_fill()
+
+# color("red")
+# begin_fill()
+# forward(50)
+# for i in range(2):
+#     right(90)
+#     forward(100)
+#     right(90)
 #     forward(50)
-#     for i in range(2):
-#         right(90)
-#         forward(100)
-#         right(90)
-#         forward(50)
-#     end_fill()
+# end_fill()
 
-color("red")
-begin_fill()
-forward(50)
-for i in range(2):
-    right(90)
-    forward(100)
-    right(90)
-    forward(50)
-end_fill()
+# color("blue")
+# begin_fill()
+# forward(50)
+# for i in range(2):
+#     right(90)
+#     forward(100)
+#     right(90)
+#     forward(50)
+# end_fill()
 
-color("blue")
-begin_fill()
-forward(50)
-for i in range(2):
-    right(90)
-    forward(100)
-    right(90)
-    forward(50)
-end_fill()
+# color("brown")
+# begin_fill()
+# forward(50)
+# for i in range(2):
+#     right(90)
+#     forward(100)
+#     right(90)
+#     forward(50)
+# end_fill()
 
-color("brown")
-begin_fill()
-forward(50)
-for i in range(2):
-    right(90)
-    forward(100)
-    right(90)
-    forward(50)
-end_fill()
+# color("yellow")
+# begin_fill()
+# forward(50)
+# for i in range(2):
+#     right(90)
+#     forward(100)
+#     right(90)
+#     forward(50)
+# end_fill()
 
-color("yellow")
-begin_fill()
-forward(50)
-for i in range(2):
-    right(90)
-    forward(100)
-    right(90)
-    forward(50)
-end_fill()
-
-color("grey")
-begin_fill()
-forward(50)
-for i in range(2):
-    right(90)
-    forward(100)
-    right(90)
-    forward(50)
-end_fill()
+# color("grey")
+# begin_fill()
+# forward(50)
+# for i in range(2):
+#     right(90)
+#     forward(100)
+#     right(90)
+#     forward(50)
+# end_fill()
 
 mainloop()
