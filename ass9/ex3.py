40f7e813b3029711a2f1f90bcb0e55266f50cb04
tungdau93 <tungdau1510@gmail.com>
2019-03-28 00:55:19 +0700

    ass9

---
+++ ass9/ex3.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+from matplotlib import pyplot
+
+uri = "mongodb://admin:admin@ds021182.mlab.com:21182/c4e"
+client = MongoClient(uri)
+db = client.c4e
+customers_collection = db["customers"]
+
+ref_events = customers_collection.find({ "ref": "events" })
+count_events = len(list(ref_events))
+print("Count the number of customers group by events:", count_events)
+ref_wom = customers_collection.find({ "ref": "wom" })
+count_wom = len(list(ref_wom))
+print("Count the number of customers group by wom:", count_wom)
+ref_ads = customers_collection.find({ "ref": "ads" })
+count_ads = len(list(ref_ads))
+print("Count the number of customers group by ads:", count_ads)
+
+count_list = [count_events, count_wom, count_ads]
+print(count_list)
+name_list = ["Events", "Wom", "Ads"]
+
+pyplot.pie(count_list, labels = name_list, autopct = "%.1f%%", shadow = True, explode = [0.02, 0.02, 0.02])
+pyplot.title("Events, Wom and Ads percentage of reference")
+pyplot.show()
---
+++ ass9/ex4.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+uri ="mongodb://admin:admin@ds021182.mlab.com:21182/c4e"
+client = MongoClient(uri)
+db = client.c4e
+rivers_collection = db["river"]
+
+continent1_list = rivers_collection.find({ "continent": "Africa" })
+for each_continent1 in continent1_list:
+    print(each_continent1)
+
+print("*" * 120)
+
+continent2_list = rivers_collection.find({ "continent": "S. America" })
+for each_continent2 in continent2_list:
+    if(each_continent2["length"] < 1000 ):
+        print(each_continent2)
